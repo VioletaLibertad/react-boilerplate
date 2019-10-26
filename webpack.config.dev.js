c766adbe877fc1dc796f3dfb8a83bb32e499b2e6
VioletaLibertad <morada.libertad@gmail.com>
2019-10-26 15:58:55 -0300

    eslint added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
-const webpack = require('webpack');
 
 module.exports = merge(baseConfig, {
   mode: 'development',
